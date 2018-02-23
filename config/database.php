fddc05219178274d05d4c0d575c82ebef209b083
Bastien De La Losa <b8delalo@gmail.com>
2018-02-23 21:16:13 +0100

    Blast unneccessary function.

--- config/database.php
+++ config/database.php
@@ -20,52 +20,6 @@ class Database{
         }
  
     }
- 
-    // get the database connection
-    /*
-    public function getConnection(){
- 
-        $this->conn = null;
- 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
-        }
- 
-        return $this->conn;
-    }
-    */
-
-//htmlspecialchars(strip_tags($this->user_name))
-    public static function where($aWhere, $sCond = 'AND')
-    {
-        $sWhere = '';
-        foreach ($aWhere as $sField => $aData) {
-            if (!empty($sWhere)) {
-                $sWhere .= ' '.$sCond.' ';
-            }
-
-            if (is_numeric($sField)) {
-                $sWhere .= $aData;
-                continue;
-            }
-
-            if (!is_array($aData)) {
-                $sData = htmlspecialchars(strip_tags($aData)); // or mysqli escape string
-            } else {
-                $aEscapedData = array();
-                foreach ($aData as $sFieldData) {
-                    $aEscapedData[] = htmlspecialchars(strip_tags($sFieldData));
-                }
-                $sData = implode('\',\'', $aEscapedData);
-            }
-
-            $sWhere .= $sField.' in (\''.$sData.'\')';
-        }
-        return $sWhere; //  arg1 in ('value1', 'value2'...) AND ...
-    }
 
     public function show_columns($table) {
 
